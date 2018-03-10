500a83de2b6472360106b56e9e2eb468c76b90d9
Dillon Jason <djasuar@gmail.com>
2018-03-10 13:08:23 -0500

    Zoom and center bracket

--- src/client/js/components/tournament/bracket.jsx
+++ src/client/js/components/tournament/bracket.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {Component} from 'react'
 import PropTypes from 'prop-types'
-import {graphql} from 'react-apollo'
+import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
-import {connect} from 'react-redux'
-import {bindActionCreators} from 'redux'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
 
 import get from 'lodash/get'
 import forEach from 'lodash/forEach'
@@ -15,148 +15,171 @@ import pick from 'lodash/pick'
 
 import Tree from 'react-d3-tree'
 
-import {Node} from './node'
+import { Node } from './node'
 
-import {toggleEditMatches} from './../../store/tournament/actions'
+import { toggleEditMatches } from './../../store/tournament/actions'
 
-import {Loading} from '../shared/loading'
+import { Loading } from '../shared/loading'
 
-const setPlayerNames = ({attributes, set, isFromWinner}) => {
-  const prefix = isFromWinner ? 'Winner Of' : 'Loser Of'
-  let playerName = `${prefix} ${get(set, 'name')}`
-  let playerReady = false
-  const winnerId = get(set, 'setWinner.id')
-
-  if (winnerId) {
-    playerReady = true
-    if (isFromWinner) {
-      playerName = get(set, 'setWinner.name')
-    } else {
-      playerName = winnerId === get(set, 'firstPlayer.id') ? get(set, 'secondPlayer.name') : get(set, 'firstPlayer.name')
+export class BracketComponent extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      translate: undefined
     }
   }
 
-  if (attributes.firstPlayer) {
-    attributes.secondPlayer = playerName
-    attributes.secondPlayerReady = playerReady
-  } else {
-    attributes.firstPlayer = playerName
-    attributes.firstPlayerReady = playerReady
-  }
-}
-
-const getSetAttributes = ({set}) => pick(set, ['id', 'name'])
-
-const generateTournamentTree = ({Tournament, set, endBranch}) => {
-  const setData = find(Tournament.sets, tournamentSet => tournamentSet.id === set.id)
-
-  if ((isEmpty(setData.winnerFromSets) && isEmpty(setData.loserFromSets)) || endBranch) {
-    return endBranch
-      ? {
-        attributes: {
-          ...getSetAttributes({set: setData}),
-          firstPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name')),
-          secondPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name'))
-        }
+  componentDidMount () {
+    const dimensions = this.treeContainer.getBoundingClientRect()
+    this.setState({
+      translate: {
+        x: dimensions.width / 2,
+        y: dimensions.height / 2
       }
-      : {
-        attributes: {
-          ...getSetAttributes({set: setData}),
-          firstPlayer: get(setData, 'matches.0.firstPlayer.name'),
-          firstPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name')),
-          secondPlayer: get(setData, 'matches.0.secondPlayer.name'),
-          secondPlayerReady: Boolean(get(setData, 'matches.0.secondPlayer.name'))
-        }
-      }
-  } else {
-    const children = []
-    const attributes = {...getSetAttributes({set: setData})}
-    const setHasBye = setData.winnerFromSets.length <= 1
-    const setLosersStart = setData.loserFromSets.length === 2
-    const numPlayers = setData.winnerFromSets.length + setData.loserFromSets.length
-
-    forEach(setData.winnerFromSets, fromSet => {
-      const fromSetData = find(Tournament.sets, tournamentSet => tournamentSet.id === fromSet.id)
-      setPlayerNames({attributes, set: fromSetData, isFromWinner: true})
-      
-      children.push({
-        name: get(fromSetData, 'name'),
-        ...generateTournamentTree({Tournament, set: fromSetData})
-      })
     })
+  }
 
-    forEach(setData.loserFromSets, fromSet => {
-      const fromSetData = find(Tournament.sets, tournamentSet => tournamentSet.id === fromSet.id)
-      setPlayerNames({attributes, set: fromSetData, isFromWinner: false})
-      
-      if (!setLosersStart && numPlayers === 2) {
+  setPlayerNames ({ attributes, set, isFromWinner }) {
+    const prefix = isFromWinner ? 'Winner Of' : 'Loser Of'
+    let playerName = `${prefix} ${get(set, 'name')}`
+    let playerReady = false
+    const winnerId = get(set, 'setWinner.id')
+  
+    if (winnerId) {
+      playerReady = true
+      if (isFromWinner) {
+        playerName = get(set, 'setWinner.name')
+      } else {
+        playerName = winnerId === get(set, 'firstPlayer.id') ? get(set, 'secondPlayer.name') : get(set, 'firstPlayer.name')
+      }
+    }
+  
+    if (attributes.firstPlayer) {
+      attributes.secondPlayer = playerName
+      attributes.secondPlayerReady = playerReady
+    } else {
+      attributes.firstPlayer = playerName
+      attributes.firstPlayerReady = playerReady
+    }
+  }
+  
+  getSetAttributes ({ set }) {
+    return pick(set, ['id', 'name'])
+  }
+  
+  generateTournamentTree ({ Tournament, set, endBranch }) {
+    const setData = find(Tournament.sets, tournamentSet => tournamentSet.id === set.id)
+  
+    if ((isEmpty(setData.winnerFromSets) && isEmpty(setData.loserFromSets)) || endBranch) {
+      return endBranch
+        ? {
+          attributes: {
+            ...this.getSetAttributes({ set: setData }),
+            firstPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name')),
+            secondPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name'))
+          }
+        }
+        : {
+          attributes: {
+            ...this.getSetAttributes({ set: setData }),
+            firstPlayer: get(setData, 'matches.0.firstPlayer.name'),
+            firstPlayerReady: Boolean(get(setData, 'matches.0.firstPlayer.name')),
+            secondPlayer: get(setData, 'matches.0.secondPlayer.name'),
+            secondPlayerReady: Boolean(get(setData, 'matches.0.secondPlayer.name'))
+          }
+        }
+    } else {
+      const children = []
+      const attributes = { ...this.getSetAttributes({ set: setData }) }
+      const setHasBye = setData.winnerFromSets.length <= 1
+      const setLosersStart = setData.loserFromSets.length === 2
+      const numPlayers = setData.winnerFromSets.length + setData.loserFromSets.length
+  
+      forEach(setData.winnerFromSets, fromSet => {
+        const fromSetData = find(Tournament.sets, tournamentSet => tournamentSet.id === fromSet.id)
+        this.setPlayerNames({ attributes, set: fromSetData, isFromWinner: true })
+  
         children.push({
           name: get(fromSetData, 'name'),
-          ...generateTournamentTree({Tournament, set: fromSetData, endBranch: setHasBye})
+          ...this.generateTournamentTree({ Tournament, set: fromSetData })
         })
-      }
-    })
-    
-    return {attributes, children}
+      })
+  
+      forEach(setData.loserFromSets, fromSet => {
+        const fromSetData = find(Tournament.sets, tournamentSet => tournamentSet.id === fromSet.id)
+        this.setPlayerNames({ attributes, set: fromSetData, isFromWinner: false })
+  
+        if (!setLosersStart && numPlayers === 2) {
+          children.push({
+            name: get(fromSetData, 'name'),
+            ...this.generateTournamentTree({ Tournament, set: fromSetData, endBranch: setHasBye })
+          })
+        }
+      })
+  
+      return { attributes, children }
+    }
   }
-}
-
-const onSetClick = ({attributes, toggleEditMatches}) => {
-  if (attributes.firstPlayerReady && attributes.secondPlayerReady) {
-    toggleEditMatches({
-      id: attributes.id,
-      name: attributes.name
-    })
-  }
-}
-
-export const BracketComponent = ({data, toggleEditMatches}) => {
-  const {loading, error, Tournament} = data
-  const treeData = []
-
-  if (Tournament) {
-    const finalSets = filter(Tournament.sets, set => !set.winnerSet && !set.loserSet)
-    const finalSet = find(finalSets, set => find(Tournament.sets, otherSet => get(otherSet, 'winnerSet.id', '') === set.id))
-
-    treeData.push({
-      name: finalSet.name,
-      ...generateTournamentTree({Tournament, set: finalSet})
-    })
+  
+  onSetClick ({ attributes, toggleEditMatches }) {
+    if (attributes.firstPlayerReady && attributes.secondPlayerReady) {
+      toggleEditMatches({
+        id: attributes.id,
+        name: attributes.name
+      })
+    }
   }
 
-  return (
-    <div style={{flex: '1'}}>
-      {loading && <Loading />}
-      {error && 'Error'}
-      {Tournament &&
-        <Tree
-          data={treeData}
-          collapsible={false}
-          pathFunc={'elbow'}
-          nodeSvgShape={{
-            shape: 'none'
-          }}
-          nodeSize={{
-            x: 200,
-            y: 150
-          }}
-          allowForeignObjects
-          nodeLabelComponent={{
-            render: <Node />,
-            foreignObjectWrapper: {
-              x: -88,
-              y: -63
-            }
-          }}
-          separation={{
-            siblings: 2,
-            nonSiblings: 3
-          }}
-          onClick={({attributes}) => onSetClick({attributes, toggleEditMatches})}
-        />
-      }
-    </div>
-  )
+  render () {
+    const { loading, error, Tournament } = this.props.data
+    const treeData = []
+
+    if (Tournament) {
+      const finalSets = filter(Tournament.sets, set => !set.winnerSet && !set.loserSet)
+      const finalSet = find(finalSets, set => find(Tournament.sets, otherSet => get(otherSet, 'winnerSet.id', '') === set.id))
+
+      treeData.push({
+        name: finalSet.name,
+        ...this.generateTournamentTree({ Tournament, set: finalSet })
+      })
+    }
+
+    return (
+      <div style={{ flex: '1' }} ref={tc => (this.treeContainer = tc)}>
+        {loading && <Loading />}
+        {error && 'Error'}
+        {Tournament &&
+          <Tree
+            data={treeData}
+            collapsible={false}
+            pathFunc={'elbow'}
+            nodeSvgShape={{
+              shape: 'none'
+            }}
+            nodeSize={{
+              x: 200,
+              y: 150
+            }}
+            allowForeignObjects
+            nodeLabelComponent={{
+              render: <Node />,
+              foreignObjectWrapper: {
+                x: -88,
+                y: -63
+              }
+            }}
+            separation={{
+              siblings: 2,
+              nonSiblings: 3
+            }}
+            zoom={0.6}
+            translate={this.state.translate}
+            onClick={({ attributes }) => this.onSetClick({ attributes, toggleEditMatches })}
+          />
+        }
+      </div>
+    )
+  }
 }
 
 BracketComponent.propTypes = {
